df9831521a7d663ff5c48be905cca0fb259c52eb
Kuroi Kyu <hello@kuroikyu.com>
2025-03-01 14:20:17 +0100

    feat: rename kensaku and adapt branding

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ function noSearchDefaultPageRender() {
   app.innerHTML = `
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
       <div class="content-container">
-        <h1>Und*ck</h1>
+        <h1>Kensaku ⋅ 検索</h1>
         <p>DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a></p>
         <div class="url-container"> 
           <input 
             type="text" 
             class="url-input"
-            value="https://unduck.link?q=%s"
+            value="https://kensaku.kuroikyu.com?q=%s"
             readonly 
           />
           <button class="copy-button">
@@ -21,11 +21,7 @@ function noSearchDefaultPageRender() {
         </div>
       </div>
       <footer class="footer">
-        <a href="https://t3.chat" target="_blank">t3.chat</a>
-        •
-        <a href="https://x.com/theo" target="_blank">theo</a>
-        •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
+        <a href="https://github.com/kuroikyu/kensaku" target="_blank">github</a>
       </footer>
     </div>
   `;
